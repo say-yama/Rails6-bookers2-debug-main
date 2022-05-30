30c4eb9415338d6cd988f0130b5d2d68572a0aea
EC2 Default User <ec2-user@ip-172-31-37-207.ec2.internal>
2022-05-30 09:01:25 +0000

    [Update]いいね機能の編集

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,5 +1,5 @@
 class BookCommentsController < ApplicationController
-  
+
   def create
     book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
@@ -7,12 +7,23 @@ class BookCommentsController < ApplicationController
     comment.save
     redirect_to book_path(book)
   end
-  
+
+  def destroy
+    BookComment.find(params[:id]).destroy
+    redirect_to book_path(params[:book_id])
+  end
+
+
+
+
+
+
+
   private
-  
+
   def book_comment_params
     params.require(:book_comment).permit(:comment)
   end
-  
+
 end
 
